c5855df52f431baf8792cee27f0cab023f8dbb70
Artem Smirnov <ac.hedgehog@gmail.com>
2012-11-25 10:27:10 +0100

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeroeswmBot::Application.config.secret_token = '2d145b767b5a5a95512f7265bf6c08cd7e6cf97c70352959cd4828a38557f5ee19f003415eddddd6fe33eba3fe3e71445e5e2672e827119747eb264e580d5fce'
