eb63de55cdc95e4d8a493b594131090e287b0f34
Artem Smirnov <ac.hedgehog@gmail.com>
2012-11-25 11:17:48 +0100

    Watir test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,20 @@
 class PagesController < ApplicationController
   def cool_ajax
     respond_to do |format|
-      format.json { render json: { html: "Щикарно!" } }
+      format.json { render json: { html: auth } }
     end
+  end
+  
+  private
+  
+  def auth
+    browser = Watir::Browser.new :firefox
+    browser.goto "http://www.heroeswm.ru"
+    browser.text_field(:name => 'login').set("Фубини")
+    browser.text_field(:name => 'pass').set("Det112358")
+    browser.input(:title => 'Войти в игру!').click
+    result = browser.url
+    browser.close
+    result
   end
 end
